327d34620f0ea6be9a156e4be0bc12fc4819bcb3
Galliom <gamaleguista@gmail.com>
2025-09-24 19:52:17 -0400

    This is pissing me off

--- bookbot.py
+++ bookbot.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath, "r", encoding="utf-8") as file:
+        return file.read()
+
+def main():
+    filepath = "books/frankenstein.txt"
+    text = get_book_text(filepath)
+    return text
+
+if __name__ == "__main__":
+    main()
